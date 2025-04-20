a26a6bac155412bd8942a55563555b441b23ecaf
Aidan <132329147+hammy412@users.noreply.github.com>
2025-04-20 01:27:41 -0400

    Initial commit

---
+++ backend/identifier.py
@@ -0,0 +1,23 @@
+from birdnetlib import Recording
+from birdnetlib.analyzer import Analyzer
+
+# Load and initialize the BirdNET-Analyzer models.
+analyzer = Analyzer()
+
+recording = Recording(
+    analyzer,
+    "sample.mp3",
+    min_conf=0.25,
+)
+
+recording.analyze()
+seen = set()
+
+print()
+for result in recording.detections:
+    commonName = result['common_name']
+    scientificName = result['scientific_name']
+    confidence = int(round(result['confidence'],2) * 100)
+    if commonName not in seen:
+        print(f"Common Name: {commonName}\nScientific Name: {scientificName}\nConfidence: {confidence}%")
+        seen.add(commonName)
